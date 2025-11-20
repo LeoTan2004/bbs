cab44b9d8e57bd1b060010f5793ab6d67a4d4c93
LeoTan2004 <3509836544@qq.com>
2025-11-20 15:37:41 +0800

    feat(wechat): Enhance WeChatAppService with error handling and logging

--- src/main/java/edu/xtu/bbs/user/service/WeChatAppService.java
+++ src/main/java/edu/xtu/bbs/user/service/WeChatAppService.java
@@ -2,9 +2,11 @@ package edu.xtu.bbs.user.service;
 
 import edu.xtu.bbs.user.config.WeChatAppConfiguration;
 import edu.xtu.bbs.user.dto.WeChatAppSessionDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+@Slf4j
 @Service
 public class WeChatAppService {
 
@@ -22,17 +24,23 @@ public class WeChatAppService {
         final String tokenUrl = weChatAppConfiguration.getTokenUrl();
         final String appId = weChatAppConfiguration.getAppId();
         final String appSecret = weChatAppConfiguration.getAppSecret();
+        WeChatAppSessionDto sessionDto;
+        try {
+            sessionDto = restClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path(tokenUrl)
+                            .queryParam("appid", appId)
+                            .queryParam("secret", appSecret)
+                            .queryParam("js_code", code)
+                            .queryParam("grant_type", DEFAULT_GRANT_TYPE)
+                            .build())
+                    .retrieve()
+                    .body(WeChatAppSessionDto.class);
+        } catch (IllegalArgumentException e) {
+            log.warn(e.getMessage(), e);
+            return null;
+        }
 
-        final WeChatAppSessionDto sessionDto = restClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path(tokenUrl)
-                        .queryParam("appid", appId)
-                        .queryParam("secret", appSecret)
-                        .queryParam("js_code", code)
-                        .queryParam("grant_type", DEFAULT_GRANT_TYPE)
-                        .build())
-                .retrieve()
-                .body(WeChatAppSessionDto.class);
 
         if (sessionDto != null) {
             return sessionDto.openid();
